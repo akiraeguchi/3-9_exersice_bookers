3dde5e7667c220cf89d8db1199bad5b4af977345
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-09-23 04:42:53 +0000

    Update20210923-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -26,6 +27,13 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully update"
+    redirect_to book_path(book)
   end
 
   def destroy
